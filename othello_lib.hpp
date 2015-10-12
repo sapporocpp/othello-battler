63c30685040e91df28637e57c7a542378b442178
maraigue <main@hhiro.net>
2015-10-13 01:04:48 +0900

    乱数を<random>のものに差し替え

--- othello_lib.hpp
+++ othello_lib.hpp
@@ -3,166 +3,166 @@
 #include <cstring>
 
 namespace Othello{
-	const char BLACK = 1; // 黒が置かれている
-	const char WHITE = 2; // 白が置かれている
-	const char EMPTY = 0; // 石は置かれていない
-	const char INVALID = 4; // 無効な座標を指定した場合など
-	const int SIZE = 8; // 盤面の大きさ
-	
-	class InvalidArgumentException{
-	public:
-		InvalidArgumentException(){}
-	};
-	
-	// 石の番号（BLACK, WHITEなど）を石の文字（'B', 'W'など）に変換
-	// 引数が不正でも特段のチェックはしていない
-	char get_piece_name(char color){
-		return ".BW"[static_cast<size_t>(color)];
-	}
-	
-	// 石の配置
-	class Placement{
-	private:
-		char places_[SIZE][SIZE];
-	public:
-		// 空の盤面を生成
-		Placement(){}
-		
-		// 初期盤面を生成
-		Placement(bool initialized){
-			if(initialized){
-				for(int i = 0; i < SIZE; ++i){
-					for(int j = 0; j < SIZE; ++j){
-						if((i == SIZE/2-1 || i == SIZE/2) && (j == SIZE/2-1 || j == SIZE/2)){
-							places_[i][j] = ((i+j)%2 ? BLACK : WHITE);
-						}else{
-							places_[i][j] = EMPTY;
-						}
-					}
-				}
-			}
-		}
-		
-		// 位置を指定して石を設置
-		// ※「設置」とは、単にその場所にその石がある状態にするだけで
-		//   ゲームのルールとして置く（裏返したりもする）わけではない
-		inline bool put(int i, int j, char piece){
-			if(i < 0 || i >= SIZE || j < 0 || j >= SIZE) return false;
-			places_[i][j] = piece;
-			return true;
-		}
-		
-		// 位置を指定して石を取得
-		inline char get(int i, int j) const{
-			if(i < 0 || i >= SIZE || j < 0 || j >= SIZE) return INVALID;
-			return places_[i][j];
-		}
-		
-		// 表示する
-		// current_r, current_cは「石がどこに置かれたか」（省略可能）
-		void display(int current_r, int current_c) const{
-			for(int i = 0; i < SIZE; ++i){
-				for(int j = 0; j <= SIZE; ++j){
-					if(i == current_r && j == current_c){
-						std::cout << '[';
-					}else if(i == current_r && j == current_c+1){
-						std::cout << ']';
-					}else{
-						std::cout << ' ';
-					}
-					if(j == SIZE) break;
-					std::cout << get_piece_name(places_[i][j]);
-				}
-				std::cout << std::endl;
-			}
-		}
-		
-		void display(void) const{
-			display(-1, -1);
-		}
-	};
-	
-	// 盤面（次がどちらの番か、などの情報含む）
-	class Board{
-	public:
-		static void error_and_throw(const char * progname, size_t error_line){
-			std::cerr << "Argument Error at line " << error_line << "." << std::endl;
-			std::cerr << "Usage: " << progname << " NonceString Turn BoardString" << std::endl;
-			std::cerr << "Turn must be 'B' OR 'W'." << std::endl;
-			std::cerr << "BoardString must be /^[.BW]{" << (SIZE * SIZE) << "}$/." << std::endl;
-			throw InvalidArgumentException();
-		}
-		
-	private:
-		Placement pl_;
-		std::string nonce_;
-		char turn_;
-		
-	public:
-		// コマンドライン引数から生成
-		Board(int argc, char ** argv){
-			if(argc != 4) error_and_throw(argv[0], __LINE__);
-			
-			// ランダムな文字列（本来答えを返すべきクライアントが答えを返すように）
-			nonce_.assign(argv[1]);
-			
-			// どちらの手番か
-			switch(argv[2][0]){
-			case 'B':
-				turn_ = BLACK;
-				break;
-			case 'W':
-				turn_ = WHITE;
-				break;
-			default:
-				error_and_throw(argv[0], __LINE__);
-				break;
-			}
-			
-			// 現在の石の配置
-			if(std::strlen(argv[3]) != SIZE * SIZE) error_and_throw(argv[0], __LINE__);
-			
-			for(int i = 0; i < SIZE * SIZE; ++i){
-				switch(argv[3][i]){
-				case 'B':
-					pl_.put(i/SIZE, i%SIZE, BLACK);
-					break;
-				case 'W':
-					pl_.put(i/SIZE, i%SIZE, WHITE);
-					break;
-				case '.':
-					pl_.put(i/SIZE, i%SIZE, EMPTY);
-					break;
-				default:
-					error_and_throw(argv[0], __LINE__);
-					break;
-				}
-			}
-		}
-		
-		// その場所に置かれている石
-		inline char place(int i, int j) const{
-			return pl_.get(i, j);
-		}
-		
-		// 石を置くことに対応する出力を行う
-		// どこにも置けない場合、引数なしで呼び出す
-		void put(int i, int j) const{
-			std::cout << nonce_ << " " << i << " " << j << std::endl;
-		}
-		
-		void put(void) const{
-			std::cout << nonce_ << std::endl;
-		}
-		
-		// 自分の石の色
-		inline char my_color(void) const{
-			return turn_;
-		}
-		
-		// 対戦相手の石の色
-		inline char opponent_color(void) const{
-			return(turn_ == BLACK ? WHITE : BLACK);
-		}
-	};
+    const char BLACK = 1; // 黒が置かれている
+    const char WHITE = 2; // 白が置かれている
+    const char EMPTY = 0; // 石は置かれていない
+    const char INVALID = 4; // 無効な座標を指定した場合など
+    const int SIZE = 8; // 盤面の大きさ
+    
+    class InvalidArgumentException{
+    public:
+        InvalidArgumentException(){}
+    };
+    
+    // 石の番号（BLACK, WHITEなど）を石の文字（'B', 'W'など）に変換
+    // 引数が不正でも特段のチェックはしていない
+    char get_piece_name(char color){
+        return ".BW"[static_cast<size_t>(color)];
+    }
+    
+    // 石の配置
+    class Placement{
+    private:
+        char places_[SIZE][SIZE];
+    public:
+        // 空の盤面を生成
+        Placement(){}
+        
+        // 初期盤面を生成
+        Placement(bool initialized){
+            if(initialized){
+                for(int i = 0; i < SIZE; ++i){
+                    for(int j = 0; j < SIZE; ++j){
+                        if((i == SIZE/2-1 || i == SIZE/2) && (j == SIZE/2-1 || j == SIZE/2)){
+                            places_[i][j] = ((i+j)%2 ? BLACK : WHITE);
+                        }else{
+                            places_[i][j] = EMPTY;
+                        }
+                    }
+                }
+            }
+        }
+        
+        // 位置を指定して石を設置
+        // ※「設置」とは、単にその場所にその石がある状態にするだけで
+        //   ゲームのルールとして置く（裏返したりもする）わけではない
+        inline bool put(int i, int j, char piece){
+            if(i < 0 || i >= SIZE || j < 0 || j >= SIZE) return false;
+            places_[i][j] = piece;
+            return true;
+        }
+        
+        // 位置を指定して石を取得
+        inline char get(int i, int j) const{
+            if(i < 0 || i >= SIZE || j < 0 || j >= SIZE) return INVALID;
+            return places_[i][j];
+        }
+        
+        // 表示する
+        // current_r, current_cは「石がどこに置かれたか」（省略可能）
+        void display(int current_r, int current_c) const{
+            for(int i = 0; i < SIZE; ++i){
+                for(int j = 0; j <= SIZE; ++j){
+                    if(i == current_r && j == current_c){
+                        std::cout << '[';
+                    }else if(i == current_r && j == current_c+1){
+                        std::cout << ']';
+                    }else{
+                        std::cout << ' ';
+                    }
+                    if(j == SIZE) break;
+                    std::cout << get_piece_name(places_[i][j]);
+                }
+                std::cout << std::endl;
+            }
+        }
+        
+        void display(void) const{
+            display(-1, -1);
+        }
+    };
+    
+    // 盤面（次がどちらの番か、などの情報含む）
+    class Board{
+    public:
+        static void error_and_throw(const char * progname, size_t error_line){
+            std::cerr << "Argument Error at line " << error_line << "." << std::endl;
+            std::cerr << "Usage: " << progname << " NonceString Turn BoardString" << std::endl;
+            std::cerr << "Turn must be 'B' OR 'W'." << std::endl;
+            std::cerr << "BoardString must be /^[.BW]{" << (SIZE * SIZE) << "}$/." << std::endl;
+            throw InvalidArgumentException();
+        }
+        
+    private:
+        Placement pl_;
+        std::string nonce_;
+        char turn_;
+        
+    public:
+        // コマンドライン引数から生成
+        Board(int argc, char ** argv){
+            if(argc != 4) error_and_throw(argv[0], __LINE__);
+            
+            // ランダムな文字列（本来答えを返すべきクライアントが答えを返すように）
+            nonce_.assign(argv[1]);
+            
+            // どちらの手番か
+            switch(argv[2][0]){
+            case 'B':
+                turn_ = BLACK;
+                break;
+            case 'W':
+                turn_ = WHITE;
+                break;
+            default:
+                error_and_throw(argv[0], __LINE__);
+                break;
+            }
+            
+            // 現在の石の配置
+            if(std::strlen(argv[3]) != SIZE * SIZE) error_and_throw(argv[0], __LINE__);
+            
+            for(int i = 0; i < SIZE * SIZE; ++i){
+                switch(argv[3][i]){
+                case 'B':
+                    pl_.put(i/SIZE, i%SIZE, BLACK);
+                    break;
+                case 'W':
+                    pl_.put(i/SIZE, i%SIZE, WHITE);
+                    break;
+                case '.':
+                    pl_.put(i/SIZE, i%SIZE, EMPTY);
+                    break;
+                default:
+                    error_and_throw(argv[0], __LINE__);
+                    break;
+                }
+            }
+        }
+        
+        // その場所に置かれている石
+        inline char place(int i, int j) const{
+            return pl_.get(i, j);
+        }
+        
+        // 石を置くことに対応する出力を行う
+        // どこにも置けない場合、引数なしで呼び出す
+        void put(int i, int j) const{
+            std::cout << nonce_ << " " << i << " " << j << std::endl;
+        }
+        
+        void put(void) const{
+            std::cout << nonce_ << std::endl;
+        }
+        
+        // 自分の石の色
+        inline char my_color(void) const{
+            return turn_;
+        }
+        
+        // 対戦相手の石の色
+        inline char opponent_color(void) const{
+            return(turn_ == BLACK ? WHITE : BLACK);
+        }
+    };
 }
--- othello_runner.cpp
+++ othello_runner.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
 #include <vector>
 #include <string>
+#include <random>
+#include <chrono>
+#include <iostream>
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
@@ -14,221 +16,218 @@
 
 const size_t BUFSIZE = 64;
 
-// 0以上n未満の整数を返す
-int rand_int(int n){
-	int result;
-	do{
-		result = std::rand() / (RAND_MAX / n);
-	}while(result >= n);
-	return result;
-}
-
 // 実行結果を読み込む。
 // 返ってくる文字列は、以下のいずれかでなければならない。
 // ・「nonce 置く場所(行) 置く場所(列)」（置ける場合）
 // ・「nonce」（どこにも置けない場合）
 bool parse_sent_string(const char * buf, std::string & nonce, int & r, int & c){
-	// まず、スペースで区切るため、スペースの位置を列挙する
-	std::vector<char *> spaces;
-	char *end_r, *end_c;
-	
-	char * pos = std::strchr(buf, ' ');
-	while(pos != NULL){
-		spaces.push_back(pos);
-		pos = std::strchr(pos + 1, ' ');
-	}
-	
-	switch(spaces.size()){
-	case 0:
-		// buf中にスペースがない＝もう置ける場所がない（buf全体がnonce）
-		nonce.assign(buf);
-		r = -1;
-		c = -1;
-		return true;
-	case 2:
-		// 石が置かれる
-		nonce.assign(buf, spaces[0] - buf);
-		r = static_cast<int>(std::strtol(spaces[0] + 1, &end_r, 10));
-		c = static_cast<int>(std::strtol(spaces[1] + 1, &end_c, 10));
-		
-		// 数値を指定すべき部分で数字以外が見つかった場合、不正なフォーマット
-		if(end_r != spaces[1] || end_c != buf + std::strlen(buf)){
-			return false;
-		}
-		
-		return true;
-	default:
-		return false;
-	}
+    // まず、スペースで区切るため、スペースの位置を列挙する
+    std::vector<char *> spaces;
+    char *end_r, *end_c;
+    
+    char * pos = std::strchr(buf, ' ');
+    while(pos != NULL){
+        spaces.push_back(pos);
+        pos = std::strchr(pos + 1, ' ');
+    }
+    
+    switch(spaces.size()){
+    case 0:
+        // buf中にスペースがない＝もう置ける場所がない（buf全体がnonce）
+        nonce.assign(buf);
+        r = -1;
+        c = -1;
+        return true;
+    case 2:
+        // 石が置かれる
+        nonce.assign(buf, spaces[0] - buf);
+        r = static_cast<int>(std::strtol(spaces[0] + 1, &end_r, 10));
+        c = static_cast<int>(std::strtol(spaces[1] + 1, &end_c, 10));
+        
+        // 数値を指定すべき部分で数字以外が見つかった場合、不正なフォーマット
+        if(end_r != spaces[1] || end_c != buf + std::strlen(buf)){
+            return false;
+        }
+        
+        return true;
+    default:
+        return false;
+    }
 }
 
 // (r, c)の場所にplayerが石を置き、裏返す。
 // 一つでも裏返したらtrue, そうでなければfalse
 bool flip_stones(Othello::Placement & placement, int r, int c, char player_color, char opponent_color){
-	// 8方向に石を伸ばしていく
-	int t;
-	bool flipped = false;
-	
-	for(int y = -1; y <= 1; ++y){
-		for(int x = -1; x <= 1; ++x){
-			if(x == 0 && y == 0) continue;
-			
-			// まず、隣が相手の石でなければ、石は裏返せない
-			if(placement.get(r + y, c + x) != opponent_color){
-				continue;
-			}
-			
-			// そして、そこから石を伸ばしていって、
-			// 対戦相手の石が続いて、そのあと自分の石が現れればよい
-			t = 2;
-			while(placement.get(r + y*t, c + x*t) == opponent_color){
-				++t;
-			}
-			
-			if(placement.get(r + y*t, c + x*t) == player_color){
-				// TODO: ここを保留する
-				// まず石を置く
-				placement.put(r, c, player_color);
-				
-				// 石を裏返す
-				for(; t >= 1; --t){
-					placement.put(r + y*t, c + x*t, player_color);
-				}
-				flipped = true;
-			}
-		}
-	}
-	
-	return flipped;
+    // 8方向に石を伸ばしていく
+    int t;
+    bool flipped = false;
+    
+    for(int y = -1; y <= 1; ++y){
+        for(int x = -1; x <= 1; ++x){
+            if(x == 0 && y == 0) continue;
+            
+            // まず、隣が相手の石でなければ、石は裏返せない
+            if(placement.get(r + y, c + x) != opponent_color){
+                continue;
+            }
+            
+            // そして、そこから石を伸ばしていって、
+            // 対戦相手の石が続いて、そのあと自分の石が現れればよい
+            t = 2;
+            while(placement.get(r + y*t, c + x*t) == opponent_color){
+                ++t;
+            }
+            
+            if(placement.get(r + y*t, c + x*t) == player_color){
+                // TODO: ここを保留する
+                // まず石を置く
+                placement.put(r, c, player_color);
+                
+                // 石を裏返す
+                for(; t >= 1; --t){
+                    placement.put(r + y*t, c + x*t, player_color);
+                }
+                flipped = true;
+            }
+        }
+    }
+    
+    return flipped;
 }
 
 // メイン
 int main(int argc, char ** argv){
-	if(argc != 3){
-		std::cerr << "Usage: " << argv[0] << " PROGRAM1 PROGRAM2" << std::endl;
-		return 1;
-	}
-	
-	std::srand(std::time(0));
-	
-	Othello::Placement placement(true);
-	
-	int player; // 1 or 2 (argv[1] / argv[2] と指定するため)
-	int opponent = 1;
-	char player_color, opponent_color;
-	
-	std::string command, nonce, received_nonce;
-	char buf[BUFSIZE];
-	int received_c = -1, received_r = -1;
-	bool last_choice_is_pass = false; // 直前がパスだったことを表す。2連続パスなら終了
-	
-	for(;;){
-		player = opponent;
-		opponent = 3 - player;
-		
-		player_color = (player == 1) ? Othello::BLACK : Othello::WHITE;
-		opponent_color = (opponent == 1) ? Othello::BLACK : Othello::WHITE;
-		
-		// 表示
-		std::cout << "----------" << std::endl;
-		placement.display(received_r, received_c);
-		std::cout << "[Press Enter Key]" << std::endl;
-		fgets(buf, BUFSIZE, stdin);
-		
-		// コマンドラインを生成
-		nonce.assign(1, 'a' + rand_int(26));
-		nonce.append(1, 'a' + rand_int(26));
-		
-		command.assign(argv[player]);
-		command.append(" ");
-		command.append(nonce); // nonce
-		command.append(" ");
-		command.append(1, (player == 1 ? 'B' : 'W')); // どちらの手番か
-		command.append(" ");
-		for(int i = 0; i < Othello::SIZE; ++i){
-			for(int j = 0; j < Othello::SIZE; ++j){
-				command.append(1, Othello::get_piece_name(placement.get(i, j)));
-			}
-		}
-		
-		// コマンドを実行し、結果を受け取る
-		FILE * fp = POPEN(command.c_str(), "r");
-		if(fp == NULL){
-			std::cerr << "[ERROR] Failed in running: \"" << command << "\"" << std::endl;
-			return 1;
-		}
-		
-		fgets(buf, BUFSIZE, fp);
-		size_t buflen = std::strlen(buf);
-		
-		// 末尾の改行を除去
-		if(buflen == 0 || buf[buflen - 1] != '\n'){
-			std::cerr << "[ERROR] Player " << player << ": Too long result received (received \"" << buf << "\") [" << static_cast<int>(buf[buflen - 1]) << "]" << std::endl;
-			return 1;
-		}
-		buf[buflen - 1] = '\0';
-		
-		// 受け取った文字列を解析
-		if(!parse_sent_string(buf, received_nonce, received_r, received_c)){
-			std::cerr << "[ERROR] Player " << player << ": Invalid format received: \"" << buf << "\" (Expected: \"NONCE ROWNUM COLNUM\")" << std::endl;
-			return 1;
-		}
-		
-		if(nonce.compare(received_nonce) != 0){
-			std::cerr << "[ERROR] Player " << player << ": Invalid nonce received: \"" << received_nonce << "\" (Expected: \"" << nonce << "\")" << std::endl;
-			return 1;
-		}
-		
-		// 有効な手が指されているか確認
-		if(received_r == -1 || received_c == -1){
-			// パスした場合
-			std::cout << "Player" << player << " passed his/her turn." << std::endl;
-			if(last_choice_is_pass) break; // 二人ともパスした場合は終了
-			last_choice_is_pass = true;
-			continue;
-		}
-		
-		std::cout << "Player" << player << " put at (" << received_r << ", " << received_c << ")." << std::endl;
-		
-		if(received_c < 0 || received_c >= Othello::SIZE || received_r < 0 || received_r >= Othello::SIZE){
-			// 座標が不正な場合（盤面の範囲外）
-			std::cerr << "[ERROR] Player " << player << ": Invalid coordinates received: " << received_r << "," << received_c << std::endl;
-			return 1;
-		}
-		
-		if(placement.get(received_r, received_c) != Othello::EMPTY){
-			// 石が置かれていない場所以外に置こうとした場合
-			std::cerr << "[ERROR] Player " << player << ": Piece already exists" << std::endl;
-			return 1;
-		}
-		
-		last_choice_is_pass = false;
-		
-		// その場所に石を置いて裏返す
-		if(!flip_stones(placement, received_r, received_c, player_color, opponent_color)){
-			// 一つも石が裏返らなかった場合は反則
-			std::cerr << "Player " << player << ": No stone flipped" << std::endl;
-			return 1;
-		}
-	}
-	
-	// 終了。結果を調べる
-	int b_count = 0, w_count = 0;
-	
-	for(int i = 0; i < Othello::SIZE; ++i){
-		for(int j = 0; j < Othello::SIZE; ++j){
-			switch(placement.get(i, j)){
-			case Othello::BLACK:
-				++b_count;
-				break;
-			case Othello::WHITE:
-				++w_count;
-				break;
-			}
-		}
-	}
-	std::cout << "Black: " << b_count << std::endl;
-	std::cout << "White: " << w_count << std::endl;
-	
-	return 0;
+    if(argc != 3){
+        std::cerr << "Usage: " << argv[0] << " PROGRAM1 PROGRAM2" << std::endl;
+        return 1;
+    }
+    
+    // 乱数初期化（nonceの生成に利用）
+    // 現在時刻の取得部分は http://kudannlab.blogspot.jp/2013/12/c11.html を参考にした
+    std::chrono::high_resolution_clock::time_point clock = std::chrono::high_resolution_clock::now();
+    long long int count = std::chrono::duration_cast<std::chrono::nanoseconds>(clock.time_since_epoch()).count();
+    std::mt19937_64 rand_engine(count);
+    std::uniform_int_distribution<char> rand_distribution('A', 'Z');
+    
+    // 盤面の状態
+    Othello::Placement placement(true);
+    
+    int player; // 1 or 2 (argv[1] / argv[2] と指定するため)
+    int opponent = 1;
+    char player_color, opponent_color;
+    
+    std::string command, nonce, received_nonce;
+    char buf[BUFSIZE];
+    int received_c = -1, received_r = -1;
+    bool last_choice_is_pass = false; // 直前がパスだったことを表す。2連続パスなら終了
+    
+    for(;;){
+        player = opponent;
+        opponent = 3 - player;
+        
+        player_color = (player == 1) ? Othello::BLACK : Othello::WHITE;
+        opponent_color = (opponent == 1) ? Othello::BLACK : Othello::WHITE;
+        
+        // 表示
+        std::cout << "----------" << std::endl;
+        placement.display(received_r, received_c);
+        std::cout << "[Press Enter Key]" << std::endl;
+        fgets(buf, BUFSIZE, stdin);
+        
+        // コマンドラインを生成
+        nonce.assign(1, rand_distribution(rand_engine));
+        nonce.append(1, rand_distribution(rand_engine));
+        
+        command.assign(argv[player]);
+        command.append(" ");
+        command.append(nonce); // nonce
+        command.append(" ");
+        command.append(1, (player == 1 ? 'B' : 'W')); // どちらの手番か
+        command.append(" ");
+        for(int i = 0; i < Othello::SIZE; ++i){
+            for(int j = 0; j < Othello::SIZE; ++j){
+                command.append(1, Othello::get_piece_name(placement.get(i, j)));
+            }
+        }
+        
+        // コマンドを実行し、結果を受け取る
+        FILE * fp = POPEN(command.c_str(), "r");
+        if(fp == NULL){
+            std::cerr << "[ERROR] Failed in running: \"" << command << "\"" << std::endl;
+            return 1;
+        }
+        
+        fgets(buf, BUFSIZE, fp);
+        size_t buflen = std::strlen(buf);
+        
+        // 末尾の改行を除去
+        if(buflen == 0 || buf[buflen - 1] != '\n'){
+            std::cerr << "[ERROR] Player " << player << ": Too long result received (received \"" << buf << "\") [" << static_cast<int>(buf[buflen - 1]) << "]" << std::endl;
+            return 1;
+        }
+        buf[buflen - 1] = '\0';
+        
+        // 受け取った文字列を解析
+        if(!parse_sent_string(buf, received_nonce, received_r, received_c)){
+            std::cerr << "[ERROR] Player " << player << ": Invalid format received: \"" << buf << "\" (Expected: \"NONCE ROWNUM COLNUM\")" << std::endl;
+            return 1;
+        }
+        
+        if(nonce.compare(received_nonce) != 0){
+            std::cerr << "[ERROR] Player " << player << ": Invalid nonce received: \"" << received_nonce << "\" (Expected: \"" << nonce << "\")" << std::endl;
+            return 1;
+        }
+        
+        // 有効な手が指されているか確認
+        if(received_r == -1 || received_c == -1){
+            // パスした場合
+            std::cout << "Player" << player << " passed his/her turn." << std::endl;
+            if(last_choice_is_pass) break; // 二人ともパスした場合は終了
+            last_choice_is_pass = true;
+            continue;
+        }
+        
+        std::cout << "Player" << player << " put at (" << received_r << ", " << received_c << ")." << std::endl;
+        
+        if(received_c < 0 || received_c >= Othello::SIZE || received_r < 0 || received_r >= Othello::SIZE){
+            // 座標が不正な場合（盤面の範囲外）
+            std::cerr << "[ERROR] Player " << player << ": Invalid coordinates received: " << received_r << "," << received_c << std::endl;
+            return 1;
+        }
+        
+        if(placement.get(received_r, received_c) != Othello::EMPTY){
+            // 石が置かれていない場所以外に置こうとした場合
+            std::cerr << "[ERROR] Player " << player << ": Piece already exists" << std::endl;
+            return 1;
+        }
+        
+        last_choice_is_pass = false;
+        
+        // その場所に石を置いて裏返す
+        if(!flip_stones(placement, received_r, received_c, player_color, opponent_color)){
+            // 一つも石が裏返らなかった場合は反則
+            std::cerr << "Player " << player << ": No stone flipped" << std::endl;
+            return 1;
+        }
+    }
+    
+    // 終了。結果を調べる
+    int b_count = 0, w_count = 0;
+    
+    for(int i = 0; i < Othello::SIZE; ++i){
+        for(int j = 0; j < Othello::SIZE; ++j){
+            switch(placement.get(i, j)){
+            case Othello::BLACK:
+                ++b_count;
+                break;
+            case Othello::WHITE:
+                ++w_count;
+                break;
+            }
+        }
+    }
+    std::cout << "Black: " << b_count << std::endl;
+    std::cout << "White: " << w_count << std::endl;
+    
+    return 0;
 }
